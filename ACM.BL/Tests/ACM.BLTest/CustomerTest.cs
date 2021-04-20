2bf9a470a9c906bc1c82a68343490ce646eac7ca
Felix Anducho <felix.anducho74@gmail.com>
2021-04-19 19:29:40 -0500

    Building Entity Classes

---
+++ ACM.BL/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = "Felix",
+                LastName = "Anducho"
+            };
+
+            string expected = "Anducho, Felix";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FirstNameEmpty()
+        {
+            Customer customer = new Customer()
+            {
+                LastName = "Anducho"
+            };
+
+            string expected = "Anducho";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void LastNameEmpty()
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = "Felix"
+            };
+
+            string expected = "Felix";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            Customer c1 = new Customer();
+            c1.FirstName = "Felix";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Argel";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Anducho";
+            Customer.InstanceCount += 1;
+
+            Assert.AreEqual(3, Customer.InstanceCount);
+
+        }
+    }
+}
---
+++ ACM.BL/Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fcef6f1-f37e-4b01-b8ca-57cb5fa34df7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
